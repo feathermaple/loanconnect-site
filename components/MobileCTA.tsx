bfd08a34d424198919bed7e4b521baa006b7e19d
feathermaple <lala890913@gmail.com>
2026-04-13 04:01:24 +0800

    CTA

--- components/MobileCTA.tsx
+++ components/MobileCTA.tsx
@@ -7,14 +7,6 @@ export default function MobileCTA() {
     <div className="fixed bottom-0 left-0 right-0 z-[999] border-t border-[#e9e2d8] bg-white md:hidden">
       <div className="flex">
 
-        {/* 電話 */}
-        <a
-          href="tel:0900000000"
-          className="flex-1 flex flex-col items-center justify-center py-3 text-xs font-semibold text-[#2f2a25]"
-        >
-          📞
-          <span className="mt-1">撥打電話</span>
-        </a>
 
         {/* LINE */}
         <a
